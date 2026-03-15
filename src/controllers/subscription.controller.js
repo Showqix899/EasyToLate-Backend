c89b5c9c83c1120f5ff73e7facbdf79787a59d1b
showqi <mshowqi12@gmail.com>
2026-03-16 02:02:05 +0600

    subscription cancelation fixed

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -6,16 +6,16 @@ import { v4 as uuidv4 } from "uuid"
 import { getCache, setCache, deleteCacheByPattern } from "../utils/cache.js"
 import bookingModel from "../models/booking.model.js"
 
-export const userData = (req,res)=>{
+export const userData = (req, res) => {
     const user = req.user
-    console.log("user data----",user);
+    console.log("user data----", user);
     return res.status(200).json({
-        user:user
+        user: user
     })
 }
 
 
-export const createSubscription = async (req,res)=>{
+export const createSubscription = async (req, res) => {
     try {
         //getting current user
         const user = req.user;
@@ -23,18 +23,110 @@ export const createSubscription = async (req,res)=>{
         //getings credentials
         const {
             subscription_style,
-        }=req.body;
+        } = req.body;
 
-        if (!user){
+        if (!user) {
             return req.status(404).json({
-                message:"user not found in request"
+                message: "user not found in request"
             })
         }
         //basic validation 
-        if (!subscription_style){
+        if (!subscription_style) {
             return res.status(400).json({
-                message:"plaease chose a subscription plane"
+                message: "plaease chose a subscription plane"
             })
+        }
+
+        //try to get users subscription 
+        const user_subscription = await Subscription.findOne({
+            "owner": user._id
+        })
+
+        if (!user_subscription) {
+
+            //getting current time 
+            const currentDate = new Date()
+            let endDate = new Date(currentDate)
+            let amount;
+
+
+
+            if (subscription_style === "monthly") {
+                endDate.setMonth(endDate.getMonth() + 1)
+                amount = Number(process.env.MONTHLY_SUBSCRIPTION_AMOUNT)
+            }
+
+            if (subscription_style === "yearly") {
+                endDate.setFullYear(endDate.getFullYear() + 1)
+                amount = Number(process.env.YEARLY_SUBSCRIPTION_AMOUNT)
+            }
+
+            //generating a transection id
+            const tran_id = uuidv4()
+
+            const data = {
+                owner: user._id,
+                owner_username: user.username,
+                owner_email: user.email,
+                subscriptionAmount: amount,
+                subscriptionStyle: subscription_style,
+                subscriptionStartingDate: currentDate,
+                subscriptionEndDate: endDate,
+                tran_id: tran_id,
+            }
+
+            const paymentData = {
+
+                total_amount: amount,
+                currency: "BDT",
+                tran_id: tran_id,
+
+                success_url: `${process.env.BASE_URL}/api/subscription/success`,
+                fail_url: `${process.env.BASE_URL}/api/subscription/fail`,
+                cancel_url: `${process.env.BASE_URL}/api/subscription/cancel`,
+                ipn_url: `${process.env.BASE_URL}/api/subscription/ipn`,
+
+                shipping_method: "online",
+                product_name: "subscription",
+                product_category: "Subscription",
+                product_profile: "general",
+
+                cus_name: user.username,
+                cus_email: user.email,
+                cus_add1: user.address,
+                cus_city: user.city || "undefined",
+                cus_country: user.country || "bangladesh",
+                cus_phone: user.phone,
+
+                //shipping info
+                ship_name: user.username,
+                ship_add1: user.address,
+                ship_city: user.city || "undefined",
+                ship_country: user.country || "bangladesh",
+                ship_postcode: 3000,
+
+            }
+
+            const is_live = false
+            //creating sslcz instance 
+            const sslcz = new SSLCommerzPayment(
+                process.env.STORE_ID,
+                process.env.STORE_PASSWORD,
+                is_live,
+            )
+
+            //initiation payment and getting api response 
+            const apiResponse = await sslcz.init(paymentData)
+
+            const paymentGateWay = apiResponse.GatewayPageURL
+
+            await Subscription.create(data)
+
+            res.status(200).json({
+                payment_url: paymentGateWay
+            })
+
+
         }
 
         //getting current time 
@@ -42,12 +134,12 @@ export const createSubscription = async (req,res)=>{
         let endDate = new Date(currentDate)
         let amount;
 
-        if (subscription_style === "monthly"){
+        if (subscription_style === "monthly") {
             endDate.setMonth(endDate.getMonth() + 1)
             amount = Number(process.env.MONTHLY_SUBSCRIPTION_AMOUNT)
         }
 
-        if (subscription_style === "yearly"){
+        if (subscription_style === "yearly") {
             endDate.setFullYear(endDate.getFullYear() + 1)
             amount = Number(process.env.YEARLY_SUBSCRIPTION_AMOUNT)
         }
@@ -55,22 +147,11 @@ export const createSubscription = async (req,res)=>{
         //generating a transection id
         const tran_id = uuidv4()
 
-        const data={
-            owner:user._id,
-            owner_username:user.username,
-            owner_email:user.email,
-            subscriptionAmount:amount,
-            subscriptionStyle:subscription_style,
-            subscriptionStartingDate:currentDate,
-            subscriptionEndDate:endDate,
-            tran_id:tran_id,
-        }
-
         const paymentData = {
 
-            total_amount:amount,
-            currency:"BDT",
-            tran_id :tran_id,
+            total_amount: amount,
+            currency: "BDT",
+            tran_id: tran_id,
 
             success_url: `${process.env.BASE_URL}/api/subscription/success`,
             fail_url: `${process.env.BASE_URL}/api/subscription/fail`,
@@ -85,7 +166,7 @@ export const createSubscription = async (req,res)=>{
             cus_name: user.username,
             cus_email: user.email,
             cus_add1: user.address,
-            cus_city:user.city || "undefined",
+            cus_city: user.city || "undefined",
             cus_country: user.country || "bangladesh",
             cus_phone: user.phone,
 
@@ -111,30 +192,39 @@ export const createSubscription = async (req,res)=>{
 
         const paymentGateWay = apiResponse.GatewayPageURL
 
-        await Subscription.create(data)
+        user_subscription.subscriptionAmount=amount
+        user_subscription.tran_id=tran_id
+        user_subscription.subscription_style=subscription_style
+        user_subscription.subscriptionStartingDate=currentDate
+        user_subscription.subscriptionEndDate=endDate
+
+        //save to db 
+        await user_subscription.save();
+
 
         res.status(200).json({
-            payment_url:paymentGateWay
+            payment_url : paymentGateWay
         })
+
 
     } catch (error) {
         res.status(500).json({
             message: error.message,
-            stack:error.stack
+            stack: error.stack
         })
     }
 }
 
 //subscription success transection
-export const subscriptionSuccess = async (req,res)=>{
+export const subscriptionSuccess = async (req, res) => {
     try {
-        
-        const {val_id,tran_id} = req.body;
+
+        const { val_id, tran_id } = req.body;
 
         //basic validation 
-        if((!val_id || !tran_id)){
+        if ((!val_id || !tran_id)) {
             return res.status(404).json({
-                message:"validation id or transection id missing"
+                message: "validation id or transection id missing"
             })
         }
 
@@ -144,47 +234,47 @@ export const subscriptionSuccess = async (req,res)=>{
         // validation url
         const validation_url =
             `https://sandbox.sslcommerz.com/validator/api/validationserverAPI.php?val_id=${val_id}&store_id=${store_id}&store_passwd=${store_passwd}&format=json`
-        
+
         const validationResponse = await axios.get(validation_url)
 
         const paymentData = validationResponse.data
 
-        if (paymentData.status !== "VALID"){
+        if (paymentData.status !== "VALID") {
             return re.status(400).json({
-                message:"payment validation failed"
+                message: "payment validation failed"
             })
         }
 
-        const subscription = await Subscription.findOne({tran_id})
+        const subscription = await Subscription.findOne({ tran_id })
 
-        
 
-        if (!subscription){
+
+        if (!subscription) {
             return res.status(404).json({
-                message:"booking not found"
+                message: "booking not found"
             })
         }
 
         const user = await User.findOne({
-            _id:subscription.owner
+            _id: subscription.owner
         })
 
-        if(!user){
+        if (!user) {
             return res.status(404).json({
-                message:"No associated user found"
+                message: "No associated user found"
             })
         }
 
         //update subscription instance
-        subscription.isPaid=true
-        subscription.val_id=val_id
-        subscription.bank_tran_id=paymentData.bank_tran_id
+        subscription.isPaid = true
+        subscription.val_id = val_id
+        subscription.bank_tran_id = paymentData.bank_tran_id
 
         //saving subscription data
         await subscription.save()
 
         //saving user instance
-        user.isSubscribed=true
+        user.isSubscribed = true
         await user.save()
 
         return res.status(200).json({
@@ -201,23 +291,23 @@ export const subscriptionSuccess = async (req,res)=>{
 
 
 //fail subscription controller
-export const subscriptionFail = async (req,res)=>{
+export const subscriptionFail = async (req, res) => {
     try {
-        
-        const {tran_id} = req.body;
 
-        if (!tran_id){
+        const { tran_id } = req.body;
+
+        if (!tran_id) {
             return res.status(400).json({
-                message:"no transection id found"
+                message: "no transection id found"
             })
         }
 
         //database query 
-        const subscription = await Subscription.findOne({tran_id})
+        const subscription = await Subscription.findOne({ tran_id })
 
-        if (!subscription){
+        if (!subscription) {
             return res.status(404).json({
-                message:"subscription is not found"
+                message: "subscription is not found"
             })
         }
 
@@ -234,51 +324,53 @@ export const subscriptionFail = async (req,res)=>{
             message: error.message,
 
         })
-    
+
     }
 }
 
 
 //cancel subscription 
-export const cancelSubscription = async (req,res)=>{
+export const cancelSubscription = async (req, res) => {
     try {
-        const {subs_id} = req.params
+        const valid_user = req.user
+
         //find the subscription 
-        const subscription = await Subscription.findById(subs_id)
+        const subscription = await Subscription.findOne({
+            "owner": valid_user._id
+        })
 
         //basic validation
-        if(!subscription){
+        if (!subscription) {
             return res.status(404).json({
-                message:"no subscription found"
+                message: "subscription not found",
+                data: subscription
             })
         }
 
-        if (subscription.isPaid === false){
-
-        
+        if (subscription.isPaid === false) {
             return res.status(400).json({
-                message:"subscription is not paid"
+                message: "subscription is not paid"
             })
         }
 
-        if (subscription.refundStatus ==="refunded" || subscription.refundStatus ==="requested"){
+        if (subscription.refundStatus === "refunded" || subscription.refundStatus === "requested") {
             return res.status(400).json({
-                message:"refund already is processing"
+                message: "refund already is processing"
             })
         }
 
         //user data
-        const user = await User.findById(user._id)
+        const user = await User.findById(valid_user._id)
 
-        if (!user){
+        if (!user) {
             return res.status(404).json({
-                message:"No user found"
+                message: "No user found"
             })
         }
 
-        if (user.isSubscribed === false){
+        if (user.isSubscribed === false) {
             return res.status(400).json({
-                message:"already unsubscribed"
+                message: "already unsubscribed"
             })
         }
 
@@ -297,7 +389,7 @@ export const cancelSubscription = async (req,res)=>{
             })
         }
 
-        if(!subscription.bank_tran_id){
+        if (!subscription.bank_tran_id) {
             return res.status(400).json({
                 message: "bank transaction id missing"
             })
@@ -305,20 +397,20 @@ export const cancelSubscription = async (req,res)=>{
 
         //deducted amount
         const deductedAmount = Math.floor(
-            booking.place_rent - (booking.place_rent * 0.05)
+            subscription.subscriptionAmount - (subscription.subscriptionAmount * 0.05)
         )
 
         const refund_url =
-            `https://sandbox.sslcommerz.com/validator/api/merchantTransIDvalidationAPI.php?bank_tran_id=${booking.bank_tran_id}&store_id=${process.env.STORE_ID}&store_passwd=${process.env.STORE_PASSWORD}&refund_amount=${deductedAmount}&refund_remarks=booking_cancel&format=json`
+            `https://sandbox.sslcommerz.com/validator/api/merchantTransIDvalidationAPI.php?bank_tran_id=${subscription.bank_tran_id}&store_id=${process.env.STORE_ID}&store_passwd=${process.env.STORE_PASSWORD}&refund_amount=${deductedAmount}&refund_remarks=booking_cancel&format=json`
 
 
 
 
 
         const refundResponse = await axios.get(refund_url)
-        
+
         const refundData = refundResponse.data
-                
+
         if (refundData.status !== "success") {
             return res.status(400).json({
                 message: "refund failed",
@@ -331,7 +423,11 @@ export const cancelSubscription = async (req,res)=>{
         subscription.refundAmount = deductedAmount
         subscription.refundRequestedAt = now
 
+        user.isSubscribed = false
+
         await subscription.save()
+
+        await user.save()
 
         return res.json({
             message: "subscription has been cancelled and refund processed",
--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -3,6 +3,7 @@ import {protect,adminAccess} from "../middlewares/auth.mddleware.js"
 import { createSubscription,
     subscriptionSuccess,
     subscriptionFail,
+    cancelSubscription,
  } from "../controllers/subscription.controller.js"
 const router = express.Router()
 
@@ -15,6 +16,9 @@ router.post("/success",subscriptionSuccess)
 
 //failed subscription
 router.post("/fail",subscriptionFail)
+
+//cancel subscription 
+router.get("/cancel/",protect,cancelSubscription)
 
 
 export default router
